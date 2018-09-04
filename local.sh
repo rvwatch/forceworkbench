37cc64a361152efe67de732b6e3f361807fd14aa
Nick Chen <nchen@salesforce.com>
2018-09-03 19:58:28 -0700

    Shorten sleep

--- local.sh
+++ local.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Tear down all existing services 
 docker-compose down --rmi all;
+
+# Rebuild the services
 docker-compose up -d;
-sleep 10; 
+
+# TEMP: Change the permission for the local socket
+sleep 5;
 docker exec forceworkbench_web_1 sh -c "chmod 777 /tmp/heroku.fcgi.8080.sock"
